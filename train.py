b42e131f68ea03bdafe57d907a06bad17dbddc2a
UltralyticsAssistant <web@ultralytics.com>
2025-10-31 09:34:50 +0000

    Auto-format by https://ultralytics.com/actions

--- train.py
+++ train.py
@@ -1,21 +1,23 @@
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 from ultralytics import YOLO
 
-if __name__ == '__main__':
-    model = YOLO(r'D:\EdgeDownload\ultralytics-main\profiles\LSDFormer.yaml')
+if __name__ == "__main__":
+    model = YOLO(r"D:\EdgeDownload\ultralytics-main\profiles\LSDFormer.yaml")
 
-    model.train(data=r'D:\EdgeDownload\ultralytics-main\profiles\dataset_profiles\HRSID.yaml',
-                task='detect',
-                imgsz=640,
-                epochs=300,
-                batch=8,
-                workers=20,
-                device='0',
-                amp=False,
-                optimizer='SGD',
-                resume=True,
-                project='run',
-                name='1',
-                seed=3407
-                )
+    model.train(
+        data=r"D:\EdgeDownload\ultralytics-main\profiles\dataset_profiles\HRSID.yaml",
+        task="detect",
+        imgsz=640,
+        epochs=300,
+        batch=8,
+        workers=20,
+        device="0",
+        amp=False,
+        optimizer="SGD",
+        resume=True,
+        project="run",
+        name="1",
+        seed=3407,
+    )
